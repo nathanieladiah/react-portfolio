3c6c9f118971cbe9b7a21908aeaa0600ac43156e
nathanieladiah <nathanieladiah@gmail.com>
2023-09-26 18:40:06 -0400

    Add card header as prop to component

--- src/components/Generic/Card/Card.jsx
+++ src/components/Generic/Card/Card.jsx
@@ -1,12 +1,18 @@
 import PropTypes from "prop-types";
 import "./card.scss";
 
-const Card = ({ children, className = "" }) => {
-  return <div className={`card ${className}`}>{children}</div>;
+const Card = ({ children, className = "", header = "" }) => {
+  return (
+    <div className={`card ${className}`}>
+      <h3 className="card__header">{header}</h3>
+      {children}
+    </div>
+  );
 };
 
 Card.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
+  header: PropTypes.string,
 };
 export default Card;
