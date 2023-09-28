b7e9ae6a310f3868e4cb9c445d97b7b98b6aaba0
nathanieladiah <nathanieladiah@gmail.com>
2023-09-27 20:46:19 -0400

    Add actual skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,26 +1,56 @@
+import { BiLogoDjango, BiLogoMongodb, BiLogoNodejs } from "react-icons/bi";
+import { DiCss3, DiJavascript1, DiPython, DiReact } from "react-icons/di";
 import Card from "../Generic/Card/Card";
 import "./skills.scss";
 
 const Skills = () => {
+  const skillsList = [
+    {
+      name: "JavaScript",
+      icon: <DiJavascript1 />,
+    },
+    {
+      name: "CSS",
+      icon: <DiCss3 />,
+    },
+    {
+      name: "Django",
+      icon: <BiLogoDjango />,
+    },
+    {
+      name: "Python",
+      icon: <DiPython />,
+    },
+    {
+      name: "React",
+      icon: <DiReact />,
+    },
+    {
+      name: "Express",
+      icon: null,
+    },
+    {
+      name: "node.js",
+      icon: <BiLogoNodejs />,
+    },
+    {
+      name: "Mongodb",
+      icon: <BiLogoMongodb />,
+    },
+  ];
   return (
     <Card className="skills">
       <h3 className="card__header">Skills</h3>
-      <div className="skills__container">
-        <ul className="skills__list">
-          <li className="skills__list--item">JavaScript</li>
-          <li className="skills__list--item">JavaScript</li>
-          <li className="skills__list--item">JavaScript</li>
-          <li className="skills__list--item">JavaScript</li>
-          <li className="skills__list--item">JavaScript</li>
-        </ul>
-        <ul className="skills__list">
-          <li className="skills__list--item">JavaScript</li>
-          <li className="skills__list--item">JavaScript</li>
-          <li className="skills__list--item">JavaScript</li>
-          <li className="skills__list--item">JavaScript</li>
-          <li className="skills__list--item">JavaScript</li>
-        </ul>
-      </div>
+      {/* <div className="skills__container"> */}
+      <ul className="skills__list">
+        {skillsList.map((skill) => (
+          <li key={skill.name} className="skills__list--item">
+            {skill.icon}
+            <span>{skill.name}</span>
+          </li>
+        ))}
+      </ul>
+      {/* </div> */}
     </Card>
   );
 };
