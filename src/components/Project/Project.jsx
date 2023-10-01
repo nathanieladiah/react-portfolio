e75cdeff5e5ab80cf02e8d013a3908d5d4dcba15
nathanieladiah <nathanieladiah@gmail.com>
2023-09-30 22:17:27 -0400

    Take out hardcoded data from render in Project cards

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -4,24 +4,53 @@ import Card from "../Generic/Card/Card";
 import "./project.scss";
 
 const Project = ({ title }) => {
+  const projectData = {
+    projectTitle: title,
+    img: projectImg,
+    tags: ["HTML", "CSS", "responsive"],
+    description:
+      "In this project, I work with HTML and CSS to create a responsive page. This page is similar with a page. The design is from devchallenge.io",
+    demoLink: null,
+    githubLink: "https://github.com/nathanieladiah/blogs",
+  };
+
+  const { projectTitle, img, tags, description, demoLink, githubLink } =
+    projectData;
+
   return (
-    <Card header={title} className="project">
+    <Card className="project">
       <div className="project__image">
-        <img src={projectImg} alt="" />
+        <img src={img} alt="" />
       </div>
       <div className="project__tags">
-        <span>#HTML</span>
-        <span>#CSS</span>
-        <span>#responsive</span>
+        {tags.map((tag) => (
+          <span key={tag}>#{tag}</span>
+        ))}
       </div>
-      <h4 className="project__title">Recipe Blog</h4>
-      <p className="project__description">
-        In this project, I work with HTML and CSS to create a responsive page.
-        This page is similar with a page. The design is from devchallenge.io
-      </p>
+      <h4 className="project__title">{projectTitle}</h4>
+      <p className="project__description">{description}</p>
       <div className="project__actions">
-        <button className="card__button">Demo</button>
-        <button className="card__button card__button--ghost">Code</button>
+        {/* <button className="card__button">Demo</button>
+        <button className="card__button card__button--ghost">Code</button> */}
+        {demoLink && (
+          <a
+            href={demoLink}
+            target="_blank"
+            className="card__button"
+            rel="noreferrer"
+          >
+            Demo
+          </a>
+        )}
+
+        <a
+          href={githubLink}
+          target="_blank"
+          className="card__button card__button--ghost"
+          rel="noreferrer"
+        >
+          Code
+        </a>
       </div>
     </Card>
   );
